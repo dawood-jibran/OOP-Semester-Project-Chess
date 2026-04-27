72a4702295b326dc27dd804ee469234518febaff
Dawood Jibran <f253002@cfd.nu.edu.pk>
2026-04-27 14:52:10 +0500

    Implement King piece class
    Replace the previous forward declaration with a King class derived from Piece. Adds a constructor that sets the king symbol based on color and implements isValidMove to allow one-square moves in any direction (including diagonals), disallow a zero-distance move, and prevent capturing a piece of the same color. Keeps other board interactions unchanged.

--- chess-game.cpp
+++ chess-game.cpp
@@ -93,7 +93,44 @@ class Pawn: public Piece
 class Knight;
 class Bishop;
 class Queen;
-class King;
+class King : public Piece 
+{
+    public:
+        King(Color c) : Piece(c, (c == WHITE) ? 'K' : 'k') {}
+
+        bool isValidMove(int sR, int sC, int eR, int eC, Piece* board[8][8]) const override 
+        {
+            int rowDiff = eR - sR;
+
+            if (rowDiff < 0) 
+            {
+                rowDiff = -rowDiff;
+            }
+
+            int colDiff = eC - sC;
+            
+            if (colDiff < 0) 
+            {
+                colDiff = -colDiff;
+            }
+
+            if (rowDiff <= 1 && colDiff <= 1) 
+            {
+                if (rowDiff == 0 && colDiff == 0) 
+                {
+                    return false;
+                }
+
+                if (board[eR][eC] != nullptr && board[eR][eC]->getColor() == this->color) 
+                {
+                return false;
+                }
+                return true;
+            }
+            
+            return false;
+    }
+};
 
 int main()
 {
