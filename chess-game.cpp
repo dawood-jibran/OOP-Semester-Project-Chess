9ec940a93e390f6985d6236bd9dc80d88d61ae64
Abdullah Matloob <abdullahntml@gmail.com>
2026-04-27 14:48:10 +0500

    added class pawn

--- chess-game.cpp
+++ chess-game.cpp
@@ -30,7 +30,36 @@ class Piece
         virtual bool isValidMove(int sR, int sC, int eR, int eC, Piece* board[8][8]) const = 0;
 };
 
-class Pawn;
+class Pawn: public Piece
+{
+    public:
+        Pawn(Color c) : Piece(c, (c == WHITE) ? 'P' : 'p') {}
+
+        bool isValidMove(int sR, int sC, int eR, int eC, Piece* board[8][8]) const override
+        {
+            int direction = (color == WHITE) ? -1 : 1;
+            
+            if (sC == eC && (eR-sR == direction))
+            {
+                if (board[eR][eC] == nullptr)
+                {
+                    return true;
+                }
+            }
+
+            bool isOneColumnAway = (eC - sC == 1) || (eC - sC == -1);
+
+            if (isOneColumnAway && (eR - sR == direction))
+            {
+                if (board[eR][eC] != nullptr && board[eR][eC]->getColor() != this->color)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+};
+
 class Rook;
 class Knight;
 class Bishop;
