0231ca1549e84fd0a3f09de5979207107edc0e1e
Abdullah Matloob <abdullahntml@gmail.com>
2026-04-27 14:49:20 +0500

    added classes rook

--- chess-game.cpp
+++ chess-game.cpp
@@ -60,7 +60,36 @@ class Pawn: public Piece
         }
 };
 
-class Rook;
+class Pawn: public Piece
+{
+    public:
+        Pawn(Color c) : Piece(c, (c == WHITE) ? 'P' : 'p') {}
+
+        bool isValidMove(int sR, int sC, int eR, int eC, Piece* board[8][8]) const override
+        {
+            int direction = (color == WHITE) ? -1 : 1;
+            
+            if (sC == eC && (eR-sR == direction))
+            {
+                if (board[eR][eC] == nullptr)
+                {
+                    return true;
+                }
+            }
+
+            bool isOneColumnAway = (eC - sC == 1) || (eC - sC == -1);
+
+            if (isOneColumnAway && (eR - sR == direction))
+            {
+                if (board[eR][eC] != nullptr && board[eR][eC]->getColor() != this->color)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+};
+
 class Knight;
 class Bishop;
 class Queen;
