1b543fe67be5bb5be0a78310392e4118c89d6b4c
Dawood Jibran <f253002@cfd.nu.edu.pk>
2026-05-07 16:36:29 +0500

    Add ChessPiece destructor/getters and piece ctors
    Implement ChessPiece::~ChessPiece() and add getters getTeam() and getPieceSymbol() to expose piece state. Also add/cleanup constructors for Bishop and King (setting piece symbols by team) and small formatting fixes in ChessGame.cpp. These changes provide proper polymorphic cleanup and accessors needed by move validation and game logic.

--- ChessGame.cpp
+++ ChessGame.cpp
@@ -6,11 +6,16 @@ int absoluteValue(int value)
 }
 
 ChessPiece::ChessPiece(TeamColor teamColor, char symbol) : team(teamColor), pieceSymbol(symbol) {}
+
 ChessPiece::~ChessPiece() {}
+
 TeamColor ChessPiece::getTeam() const { return team; }
+
 char ChessPiece::getPieceSymbol() const { return pieceSymbol; }
 
+
 Bishop::Bishop(TeamColor color) : ChessPiece(color, (color == WHITE) ? 'B' : 'b') {}
+
 bool Bishop::isValidMove(int startRow, int startCol, int endRow, int endCol, ChessPiece* grid[8][8]) const 
 {
     if (absoluteValue(endRow - startRow) != absoluteValue(endCol - startCol)) return false;
@@ -29,6 +34,7 @@ bool Bishop::isValidMove(int startRow, int startCol, int endRow, int endCol, Che
 
 
 King::King(TeamColor color) : ChessPiece(color, (color == WHITE) ? 'K' : 'k') {}
+
 bool King::isValidMove(int startRow, int startCol, int endRow, int endCol, ChessPiece* grid[8][8]) const 
 {
     return (absoluteValue(endRow - startRow) <= 1 && absoluteValue(endCol - startCol) <= 1) && (grid[endRow][endCol] == nullptr || grid[endRow][endCol]->getTeam() != team);
