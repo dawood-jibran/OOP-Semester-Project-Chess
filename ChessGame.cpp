13a714f3daaae75498867d97ccb5c7b5870af009
Abdullah Matloob <f253078@cfd.nu.edu.pk>
2026-05-07 16:31:23 +0500

    Update ChessGame.cpp

--- ChessGame.cpp
+++ ChessGame.cpp
@@ -1,1 +1,11 @@
 #include "ChessGame.h"
+
+int absoluteValue(int value) 
+{
+    return (value < 0) ? -value : value;
+}
+
+ChessPiece::ChessPiece(TeamColor teamColor, char symbol) : team(teamColor), pieceSymbol(symbol) {}
+ChessPiece::~ChessPiece() {}
+TeamColor ChessPiece::getTeam() const { return team; }
+char ChessPiece::getPieceSymbol() const { return pieceSymbol; }
