7a0d746923a8965957a853c2d01862f103f1f6e0
Abdullah Matloob <f253078@cfd.nu.edu.pk>
2026-05-07 16:35:08 +0500

    Update ChessGame.cpp

--- ChessGame.cpp
+++ ChessGame.cpp
@@ -9,3 +9,27 @@ ChessPiece::ChessPiece(TeamColor teamColor, char symbol) : team(teamColor), piec
 ChessPiece::~ChessPiece() {}
 TeamColor ChessPiece::getTeam() const { return team; }
 char ChessPiece::getPieceSymbol() const { return pieceSymbol; }
+
+Bishop::Bishop(TeamColor color) : ChessPiece(color, (color == WHITE) ? 'B' : 'b') {}
+bool Bishop::isValidMove(int startRow, int startCol, int endRow, int endCol, ChessPiece* grid[8][8]) const 
+{
+    if (absoluteValue(endRow - startRow) != absoluteValue(endCol - startCol)) return false;
+    int rowDir = (endRow > startRow) ? 1 : -1;
+    int colDir = (endCol > startCol) ? 1 : -1;
+    int currR = startRow + rowDir;
+    int currC = startCol + colDir;
+    while (currR != endRow) 
+    {
+        if (grid[currR][currC] != nullptr) return false; 
+        currR += rowDir;
+        currC += colDir;
+    }
+    return (grid[endRow][endCol] == nullptr || grid[endRow][endCol]->getTeam() != team);
+}
+
+
+King::King(TeamColor color) : ChessPiece(color, (color == WHITE) ? 'K' : 'k') {}
+bool King::isValidMove(int startRow, int startCol, int endRow, int endCol, ChessPiece* grid[8][8]) const 
+{
+    return (absoluteValue(endRow - startRow) <= 1 && absoluteValue(endCol - startCol) <= 1) && (grid[endRow][endCol] == nullptr || grid[endRow][endCol]->getTeam() != team);
+}
